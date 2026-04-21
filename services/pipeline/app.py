08f7d46223665e9de01c65bfc996cebfc37fe54e
zaqks <imzakbro@gmail.com>
2026-04-21 03:54:51 +0100

    flow order

--- services/pipeline/app.py
+++ services/pipeline/app.py
@@ -13,7 +13,6 @@
 
 # from src.flows.analysis.analysis_llm import *
 # from src.flows.gen.report_generation import *
-from src.flows.workspace.clean_workspace import *
 from src.flows.workspace.setup_workspace import *
 from src.flows.georef.flow_1_georef import *
 from src.flows.vecto.flow_2_vectorization_line import *
@@ -21,6 +20,7 @@ from src.flows.vecto.flow_2_vectorization_dotted import *
 from src.flows.vecto.flow_2_vectorization_poly import *
 from src.flows.cleaning.flow_3_clean_gapfill import *
 from src.flows.cleaning.flow_3_clean_noise_poly import *
+from src.flows.workspace.clean_workspace import *
 
 import uvicorn
 import os
