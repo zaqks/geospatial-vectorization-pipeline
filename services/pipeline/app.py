d38c79c4ba467497d0a2da84ee12cb28b2f3f9da
zaqks <imzakbro@gmail.com>
2026-04-22 08:49:54 +0100

    flow order

--- services/pipeline/app.py
+++ services/pipeline/app.py
@@ -1,25 +1,10 @@
-# from src.example import *
-# from src.ingestion_vldb_sigmod import *
-
-# from src.flows.ingestion.ingestion_arxiv import *
-# from src.flows.ingestion.ingestion_huggingface import *
-# from src.flows.ingestion.ingestion_nvidia import *
-
-# from src.flows.ingestion.ingestion_databricks import *
-
-# # from src.flows.ingestion.ingestion_paperswithcode import *
-# # from src.flows.ingestion.ingestion_semanticscholar import *
-# from src.flows.ingestion.ingestion_snowflake import *  # empty data
-
-# from src.flows.analysis.analysis_llm import *
-# from src.flows.gen.report_generation import *
 from src.flows.workspace.setup_workspace import *
 from src.flows.georef.flow_1_georef import *
 from src.flows.vecto.flow_2_vectorization_line import *
 from src.flows.vecto.flow_2_vectorization_dotted import *
 from src.flows.vecto.flow_2_vectorization_poly import *
-from src.flows.cleaning.flow_3_clean_gapfill import *
 from src.flows.cleaning.flow_3_clean_noise_poly import *
+from src.flows.cleaning.flow_3_clean_gapfill import *
 from src.flows.viz.flow_4_viz_line import *
 from src.flows.viz.flow_4_viz_poly import *
 from src.flows.workspace.flow_5_export_output import *
